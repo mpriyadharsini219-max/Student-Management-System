7768ec293e3a5ec12a15dfbd2da5066901e59d79
mpriyadharsini219-max <mpriyadharsini219@gmail.com>
2026-05-03 12:18:45 +0530

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,15 @@
+class Student {
+    int id;
+    String name;
+    int marks;
+
+    Student(int id, String name, int marks) {
+        this.id = id;
+        this.name = name;
+        this.marks = marks;
+    }
+
+    void display() {
+        System.out.println(id + " " + name + " " + marks);
+    }
+}
